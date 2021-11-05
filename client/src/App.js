9b78f57531f0c150f3bdb50066f7732d953dabe2
Sourabh Kulkarni <sourabhk1998@gmail.com>
2021-11-06 00:43:50 +0530

    React components added along with styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import Form from './components/Form/Form'
+import Posts from './components/Posts/Posts'
+import useStyles from './styles';
 
+import nostalgia from './images/nostalgia.png';
 const App = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth='lg'>
+            <AppBar className={classes.appBar} position='static' color='inherit'>
+                <Typography className={classes.heading} variant='h2' align='center'>Nostalgia</Typography>
+                <img src={nostalgia} alt='nostalgia' height='60' />
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify='space-between' alignItems='stretch' spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form/>
+                        </Grid>
+
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
