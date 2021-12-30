171801fe440ff79c419155f4b9fca1c5693d94f6
Sourabh Kulkarni <sourabhk1998@gmail.com>
2021-12-30 13:20:47 +0530

    Action constants added and .env created

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,12 +1,12 @@
 import * as api from '../api';
-
+import {CREATE, UPDATE, DELETE, FETCH_ALL, LIKE} from '../constants/actionTypes';
 //Action creators
 
 export const getPosts = () => async(dispatch)=> {
     try {
         const {data} = await api.fetchPosts();
 
-        dispatch({type : 'FETCH_ALL', payload: data});
+        dispatch({type : FETCH_ALL, payload: data});
     } catch (error) {
         console.log(error.message);
     }
@@ -16,7 +16,7 @@ export const createPost = (post) => async(dispatch) => {
     try {
         const {data} = await api.createPost(post);
         
-        dispatch({type: 'CREATE', payload: data})
+        dispatch({type: CREATE, payload: data})
     } catch (error) {
         console.log(error);
     }
@@ -26,7 +26,7 @@ export const updatePost = (post) => async(dispatch) => {
     try {
         const {data} = await api.updatePost(post);
         
-        dispatch({type: 'UPDATE', payload: data})
+        dispatch({type: UPDATE, payload: data})
     } catch (error) {
         console.log(error);
     }
@@ -36,7 +36,7 @@ export const deletePost = (id) => async(dispatch) => {
     try {
         await api.deletePost(id);
         
-        dispatch({type: 'DELETE', payload: id})
+        dispatch({type: DELETE, payload: id})
     } catch (error) {
         console.log(error);
     }
@@ -46,7 +46,7 @@ export const likePost = (id) => async(dispatch) => {
     try {
         const {data} = await api.likePost(id);
         
-        dispatch({type: 'LIKE', payload: data})
+        dispatch({type: UPDATE, payload: data})
     } catch (error) {
         console.log(error);
     }
--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js'
+
 const app = express();
+dotenv.config();
 
 app.use(express.json({'limit': "30mb", 'extended': true}));
 app.use(express.urlencoded({'limit': "30mb", 'extended': true}));
@@ -12,11 +14,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://nostalgia:nostalgia123@cluster0.iwc06.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
 const PORT = process.env.PORT || 5000;
  
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
     .then(()=>app.listen(PORT, () => console.log(`Server running on port : ${PORT}`)))
     .catch((error)=> console.log(error.message));
 
